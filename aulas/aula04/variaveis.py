b1c9b855bd822321264d67f145b87ae69fb5c437
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2026-04-27 11:13:21 -0300

    core: Cria arquivos variaveis.py

---
+++ aulas/aula04/variaveis.py
@@ -0,0 +1,17 @@
+# Declaração de variáveis
+
+texto = 'Este é outro texto.' # String
+numero_inteiro = 18 # Integer
+# Numero_inteiro = 18 # Integer
+numero_decimal = 7.5 # Float
+programador = True # Booleano
+
+print(texto)
+"""print(numero_inteiro)
+print(numero_decimal)
+print(programador)"""
+
+print("Valor da variável: ", texto, "\nTipo da variável: ", type(texto))
+print("Valor da variável: ", numero_inteiro, "\nTipo da variável: ", type(numero_inteiro))
+print("Valor da variável: ", numero_decimal,"\nTipo da variável: ", type(numero_decimal))
+print("Valor da variável: ", programador,"\nTipo de variável: ", type(programador))
